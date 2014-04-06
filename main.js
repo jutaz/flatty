33374f816a89b15ea4c76662919d7090c775ffba
Justas Brazauskas <brazauskasjustas@gmail.com>
2014-04-06 14:12:28 +0300

    Linted stuff and added findRecursive method.
    If there is a need to use less memory, flatty can fall back to
    recursive search, which is much slower on big databases.

--- main.js
+++ main.js
@@ -3,14 +3,14 @@ var path = require("path");
 var rand = require("generate-key");
 
 function engine(file, options) {
-  if(!options) {
+  if (!options) {
     options = {};
   }
   this.file = file;
-  if(!fs.existsSync(this.file)) {
+  if (!fs.existsSync(this.file)) {
     fs.writeFileSync(this.file, "{}");
   }
-  if(path.extname(this.file) === ".json") {
+  if (path.extname(this.file) === ".json") {
     this.data = JSON.parse(fs.readFileSync(this.file));
   } else {
     this.data = this.parse(fs.readFileSync(this.file));
@@ -19,45 +19,50 @@ function engine(file, options) {
   this.buildIndex();
   this.changes = 0;
   this.tickInterval = options.interval || 50;
+  this.options = options;
 }
 
 engine.prototype.buildIndex = function() {
-  this.indexed = {};
-  for(var i in this.data) {
-    this.index(i);
+  if (this.options.buildIndex) {
+    this.indexed = {};
+    for (var i in this.data) {
+      this.index(i);
+    }
+  } else {
+    this.find = this.findRecursive;
   }
 }
 
 engine.prototype.index = function(id, callback) {
   collection = this.data[id];
-  for(var i in collection) {
-    if(i === "id") {
+  for (var i in collection) {
+    if (i === "id") {
       continue;
     }
-    if('object' !== typeof this.indexed[i]) {
+    if ('object' !== typeof this.indexed[i]) {
       this.indexed[i] = {};
     }
-    if(this.indexed[i][collection[i]] instanceof Array) {
+    if (this.indexed[i][collection[i]] instanceof Array) {
       this.indexed[i][collection[i]] = [];
     }
-    if('string' === typeof collection[i] && collection[i].length < 1024) {
+    if ('string' === typeof collection[i] && collection[i].length < 1024) {
       this.indexed[i][collection[i]].push(id);
-    } else if('number' === typeof collection[i]) {
+    } else if ('number' === typeof collection[i]) {
       this.indexed[i][collection[i]].push(id);
     }
   }
 }
 
 engine.prototype.get = function(key, callback) {
-  if('function' == typeof key) {
+  if ('function' == typeof key) {
     callback = key;
     key = null;
   }
-  if(this.data[key] && 'object' == typeof this.data[key] || key === null) {
-    if(key === null) {
+  if (this.data[key] && 'object' == typeof this.data[key] || key === null) {
+    if (key === null) {
       arr = [];
       data = this.data;
-      for(var i in data) {
+      for (var i in data) {
         data[i].id = i;
         arr.push(data[i]);
       }
@@ -72,12 +77,12 @@ engine.prototype.get = function(key, callback) {
 }
 
 engine.prototype.set = function(key, data, callback) {
-  if('function' == typeof data) {
+  if ('function' == typeof data) {
     callback = data;
     data = key;
     key = rand.generateKey();
   }
-  if(this.data[key] && 'object' == typeof this.data[key]) {
+  if (this.data[key] && 'object' == typeof this.data[key]) {
     this.update(key, data, callback);
     return;
   }
@@ -88,7 +93,7 @@ engine.prototype.set = function(key, data, callback) {
 }
 
 engine.prototype.update = function(key, data, callback) {
-  for(var i in data) {
+  for (var i in data) {
     this.data[key][i] = data[i];
   }
   callback && callback(key);
@@ -96,10 +101,10 @@ engine.prototype.update = function(key, data, callback) {
 }
 
 engine.prototype.delete = function(key, callback) {
-  if(this.data[key] && 'object' == typeof this.data[key]) {
+  if (this.data[key] && 'object' == typeof this.data[key]) {
     this.data[key] = null;
     delete this.data[key];
-  } else if('function' === typeof key) {
+  } else if ('function' === typeof key) {
     this.data = {};
     this.indexed = {};
     callback = key;
@@ -112,33 +117,51 @@ engine.prototype.find = function(obj, callback) {
   ret = [];
   encounters = {};
   search = Object.keys(obj);
-  for(var i in search) {
-    if(this.indexed[search[i]] && this.indexed[search[i]][obj[search[i]]]) {
-      for(var s in this.indexed[search[i]][obj[search[i]]]) {
-        if(!encounters[this.indexed[search[i]][obj[search[i]]][s]]) {
+  for (var i in search) {
+    if (this.indexed[search[i]] && this.indexed[search[i]][obj[search[i]]]) {
+      for (var s in this.indexed[search[i]][obj[search[i]]]) {
+        if (!encounters[this.indexed[search[i]][obj[search[i]]][s]]) {
           encounters[this.indexed[search[i]][obj[search[i]]][s]] = 0;
         }
         encounters[this.indexed[search[i]][obj[search[i]]][s]]++;
       }
     }
   }
-  for(var e in encounters) {
-    if(encounters[e] === search.length) {
+  for (var e in encounters) {
+    if (encounters[e] === search.length) {
       ret.push(this.data[e]);
     }
   }
   callback && callback((ret.length > 0) ? ret : null);
 }
 
+engine.prototype.findRecursive = function(obj, callback) {
+  ret = [];
+  for (var i in this.data) {
+    matches = 0;
+    for (var e in obj) {
+      if (this.data[i][e] == obj[e]) {
+        matches++;
+        if (matches === Object.keys(obj).length) {
+          dat = this.data[i];
+          dat.id = i;
+          ret.push(dat);
+        }
+      }
+    }
+  }
+  callback && callback((ret.length > 0) ? ret : null)
+}
+
 engine.prototype.ticker = function() {
   this.locked = false;
   this.tick = setInterval(function() {
-    if(this.locked || this.changes == 0) {
+    if (this.locked || this.changes == 0) {
       return;
     }
     this.locked = true;
     fs.writeFile(this.file, this.stringify(this.data), function(err) {
-      if(err) {
+      if (err) {
         throw new Error(err);
       }
       this.locked = false;
@@ -148,10 +171,10 @@ engine.prototype.ticker = function() {
   this.tick.unref();
 }
 
-engine.prototype.stringify  = function(data, callback) {
+engine.prototype.stringify = function(data, callback) {
   processed = "";
-  for(var i in data) {
-    processed += i+"\t"+JSON.stringify(data[i])+"\n";
+  for (var i in data) {
+    processed += i + "\t" + JSON.stringify(data[i]) + "\n";
   }
   callback && callback(processed);
   return processed;
@@ -160,8 +183,8 @@ engine.prototype.stringify  = function(data, callback) {
 engine.prototype.parse = function(data, callback) {
   parsed = {};
   splitted = data.toString().split("\n");
-  for(var i in splitted) {
-    if(splitted[i] === '') {
+  for (var i in splitted) {
+    if (splitted[i] === '') {
       continue;
     }
     spl = splitted[i].split("\t");
