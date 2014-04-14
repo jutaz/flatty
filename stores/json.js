104d11f89765345049eb3e1a38ca89b12e01d84a
Justas Brazauskas <brazauskasjustas@gmail.com>
2014-04-14 20:14:12 +0300

    Added JSON store backbone

---
+++ stores/json.js
@@ -0,0 +1,17 @@
+function jsonStore() {
+
+}
+
+jsonStore.prototype.init = function() {
+  
+}
+
+jsonStore.prototype.stringify = function() {
+
+}
+
+jsonStore.prototype.parse = function() {
+
+}
+
+module.exports = jsonStore;
