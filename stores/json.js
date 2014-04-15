46ca7d900fe0c666ba0eedc6b392caf45cd8686d
Justas Brazauskas <brazauskasjustas@gmail.com>
2014-04-15 17:57:48 +0300

    Implemented json store

--- stores/json.js
+++ stores/json.js
@@ -3,15 +3,27 @@ function jsonStore() {
 }
 
 jsonStore.prototype.init = function() {
-  
+  return "{}";
 }
 
-jsonStore.prototype.stringify = function() {
-
+jsonStore.prototype.stringify = function(data, callback) {
+  try {
+    data = JSON.stringify(data);
+  } catch (e) {
+    throw new Error('Invalid data passed to json store when stringifying data.');
+  }
+  callback && callback(data);
+  return data;
 }
 
-jsonStore.prototype.parse = function() {
-
+jsonStore.prototype.parse = function(data, callback) {
+  try {
+    data = JSON.parse(data);
+  } catch (e) {
+    throw new Error('Invalid data passed to json store when parsing data.');
+  }
+  callback && callback(data);
+  return data;
 }
 
 module.exports = jsonStore;
