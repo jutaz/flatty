fb02ddcf8a627c7ab3c1c5fc8e0d754517e255ac
Justas Brazauskas <brazauskasjustas@gmail.com>
2014-04-17 13:37:13 +0300

    Provide switch between native and non-native libs

---
+++ main.js
@@ -0,0 +1,18 @@
+var native = require("./lib/native");
+var javascript = require("./lib/javascript");
+
+function flatty(file, options) {
+  if(!options) {
+    options = {};
+  }
+  if(!options.native) {
+    options.native = true;
+  }
+  if(!options.native) {
+    return new javascript(file, options);
+  } else {
+    return new native(file, options);
+  }
+}
+
+module.exports = flatty;
